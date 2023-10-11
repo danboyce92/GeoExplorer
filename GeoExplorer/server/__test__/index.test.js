10ef6207bbd4ef57275fa656ece5c583ebc85f54
Daniel B <daarf92@gmail.com>
2023-10-11 20:05:35 +0100

    Express jest tests added

---
+++ GeoExplorer/server/__test__/index.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const express = require('express');
+const app = require('../src/index');
+
+describe('Country search API server', () => {
+  it('should respond with country data when a valid country is provided', async () => {
+    const response = await request(app).get('/name?country=Canada');
+
+    expect(response.status).toBe(200);
+    expect(response.body[0].name.common).toBe('Canada');
+  });
+
+  it('should respond with a 400 status and an error message when no country is provided', async () => {
+    const response = await request(app).get('/name?country=');
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ error: 'Country name is required' });
+  });
+
+  it('should respond with an error message when an invalid country is provided', async () => {
+    const response = await request(app).get('/name?country=InvalidCountryName');
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ error: 'Internal server error' });
+  });
+});
--- GeoExplorer/server/src/index.js
+++ GeoExplorer/server/src/index.js
@@ -12,27 +12,29 @@ app.use(
 );
 
 app.get('/name', async (req, res) => {
-  try {
-    const countryName = req.query.country;
+  const countryName = req.query.country;
 
+  //Edge case
+  if (!countryName) {
+    return res.status(400).json({ error: 'Country name is required' });
+  }
+
+  try {
     // Make a request to the external API with the user's requested country name
     const response = await axios.get(
       `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
     );
-
-    //Potential error situations
-    if (!countryName) {
-      return res.status(400).json({ error: 'Country name is required' });
-    }
-
     // Send the fetched data as a JSON response
     const countryData = await response.data;
     res.json(countryData);
   } catch (error) {
-    res.status(error.response.status).json({ error: 'Internal server error' });
+    console.error('An error occurred:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
